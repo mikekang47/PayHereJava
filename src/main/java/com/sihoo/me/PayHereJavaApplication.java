41e2a6ce691eb534d3bd48d3e8d72c1560123f7b
mikekang47 <gyeong0308@gmail.com>
2023-01-21 18:02:04 +0900

    Init project

---
+++ src/main/java/com/sihoo/me/PayHereJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sihoo.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayHereJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayHereJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sihoo/me/PayHereJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihoo.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayHereJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
